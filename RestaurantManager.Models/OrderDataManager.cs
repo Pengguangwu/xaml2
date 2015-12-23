7ec4b0fa2a4e24f78ba60a4197dd487e2ee9dab0
Pengguang Wu <pgwu@hotmail.com>
2015-12-23 12:49:10 -0800

    Last check Dec23

---
+++ RestaurantManager.Models/OrderDataManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+
+namespace RestaurantManager.Models
+{
+    public class OrderDataManager : DataManager
+    {
+        
+        protected override void OnDataLoaded()
+
+        {
+
+   
+            this.MenuItems = base.Repository.StandardMenuItems;
+
+            this.CurrentlySelectedMenuItems = new List<MenuItem>
+            {
+                this.MenuItems[3],
+                this.MenuItems[5]
+            };
+        }
+        
+
+        public List<MenuItem> MenuItems
+        {
+            get {
+                return base.Repository.StandardMenuItems;
+            }
+
+            set { OnPropertyChanged(); }
+        }
+
+        public List<MenuItem> CurrentlySelectedMenuItems
+        {
+            get
+            {
+                this.MenuItems = base.Repository.StandardMenuItems;
+                if (MenuItems != null)
+                {
+                    return new List<MenuItem>
+            {
+                this.MenuItems[3],
+                this.MenuItems[5]
+            };
+                }
+                else
+                    return null;
+            }
+            
+            set { OnPropertyChanged(); }
+        }  
+           
+       
+    }
+}
